d6a3b951118f789aef98d66b3a6d2b6d365b5724
maanethdesilva <maaneth.2002@gmail.com>
2022-10-15 01:51:13 -0400

    Added height prop to search bar

--- Components/SearchBar/SearchBar.js
+++ Components/SearchBar/SearchBar.js
@@ -1,29 +1,36 @@
 import { StyleSheet, TextInput, View } from 'react-native';
 
-function SearchBar() {
+function SearchBar(props) {
+  const height = props.height;
+  
+  const styles = StyleSheet.create({
+    button: {
+      paddingLeft: '50px',
+      paddingRight: '50px',
+      paddingVertical: '10px',
+      backgroundColor: '#D9D9D9',
+      borderRadius: '25px',
+    },
+    text: {
+      color: '#5B5B5B',
+      fontSize: '20px',
+      fontWeight: 'Medium',
+      height: height,
+    }
+  });
+  
   return (
     <View style={styles.button}>
       <TextInput
         style={styles.text}
         placeholder='Search challenges'
+        multiline
       />
     </View>
   );
+  
 }
 
-const styles = StyleSheet.create({
-  button: {
-    paddingLeft: '50px',
-    paddingRight: '50px',
-    paddingVertical: '10px',
-    backgroundColor: '#D9D9D9',
-    borderRadius: '25px',
-  },
-  text: {
-    color: '#5B5B5B',
-    fontSize: '20px',
-    fontWeight: 'Medium',
-  }
-});
+
 
 export default SearchBar;
