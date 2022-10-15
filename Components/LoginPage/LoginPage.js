fe903e96654dd5a2d0c58b2845d13aa19e0a1f5e
your_username <arielleramgoolie27@gmail.com>
2022-10-15 09:27:44 -0400

    updated HTV button, and login

--- Components/LoginPage/LoginPage.js
+++ Components/LoginPage/LoginPage.js
@@ -1,32 +1,52 @@
 // import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, {useState} from 'react';
 import { Image, Text, View, StyleSheet, TextInput } from 'react-native';
-// import { TextInput } from 'react-native-web';
 import HTVButton from '../HTVButton/HTVButton';
 import HTVTextBox from '../HTVTextBox/HTVTextBox';
 
 
 function LoginPage() {
   const logo = require ('../../assets/Logo.png');
+  const [username, setUsername] = useState ('');
+  const [password, setPassword] = useState ('');
+
+  const currentFieldEditor = React.createRef();
+  
+  sumbitValues = () => {
+    alert(currentFieldEditor.current.this.state.name)
+  }
+
   return (
     <View style={styles.container}>
-      <Image source={ logo } style={{width: 100, height: 100}} />
-      <Text style= {{fontSize: '15px', color : '#4E8A2B', margin: 5}}>
+      <Image 
+        source={ logo } style={{width: 100, height: 100}}
+      />
+
+      <Text style= {{fontSize: '15px', color : '#33601A', marginTop: 15}}>
         Sign into your account
       </Text>
+
       <View style= {styles.input}>
-        <HTVTextBox placeholder='Enter Username' />
+        <HTVTextBox placeholder='Enter Username' ref={this.currentFieldEditor}/>
         <HTVTextBox placeholder='Enter Password' />
-        <HTVButton  buttonText = 'Sign In'/>
       </View>
+
+      <View style= {styles.input}>
+        <HTVButton  buttonText = 'Sign In' />
+      </View>
+
+      <Text style= {{fontSize: '15px', color : '#33601A', marginTop: 20}}>
+        Don't have an account? Register here!
+      </Text>
+
     </View>
+
   );
 }
 
 const styles = StyleSheet.create({
   input : {
-    marginVertical : 15,
-    justifyContent: 'center',
+    marginTop : 20,
   },
   container: {
     flex: 1,
